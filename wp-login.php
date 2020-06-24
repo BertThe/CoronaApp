2cfa26518b504827cd28bf5685f26538da86d9e1
BertThe <67274532+BertThe@users.noreply.github.com>
2020-06-24 16:05:25 +0200

    Version 2

---
+++ wp-login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="nl">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="nl">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Inloggen &lsaquo; De Corona App &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://bertthe.github.io/CoronaApp/wp-includes/css/dashicons.min.css?ver=5.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://bertthe.github.io/CoronaApp/wp-includes/css/buttons.min.css?ver=5.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://bertthe.github.io/CoronaApp/wp-admin/css/forms.min.css?ver=5.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://bertthe.github.io/CoronaApp/wp-admin/css/l10n.min.css?ver=5.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://bertthe.github.io/CoronaApp/wp-admin/css/login.min.css?ver=5.4.2' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-nl-nl-formal">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://nl.wordpress.org/">Mogelijk gemaakt door WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://bertthe.github.io/CoronaApp/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Gebruikersnaam of e-mailadres</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Wachtwoord</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Wachtwoord tonen">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Onthoud mij</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Inloggen" />
+									<input type="hidden" name="redirect_to" value="https://bertthe.github.io/CoronaApp/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+									<a href="https://bertthe.github.io/CoronaApp/wp-login.php?action=lostpassword">Wachtwoord vergeten?</a>
+								</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="https://bertthe.github.io/CoronaApp/">
+		&larr; Terug naar De Corona App		</a></p>
+			</div>
+	<script type='text/javascript' src='https://bertthe.github.io/CoronaApp/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='https://bertthe.github.io/CoronaApp/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/bertthe.github.io\/CoronaApp\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://bertthe.github.io/CoronaApp/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Sterkte wachtwoord onbekend","short":"Erg zwak","bad":"Zwak","good":"Gemiddeld","strong":"Sterk","mismatch":"Verkeerde combinatie"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://bertthe.github.io/CoronaApp/wp-admin/js/password-strength-meter.min.js?ver=5.4.2'></script>
+<script type='text/javascript' src='https://bertthe.github.io/CoronaApp/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/retriva\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://bertthe.github.io/CoronaApp/wp-includes/js/wp-util.min.js?ver=5.4.2'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var userProfileL10n = {"warn":"Uw nieuwe wachtwoord is niet opgeslagen.","warnWeak":"Bevestig het gebruik van een zwak wachtwoord","show":"Tonen","hide":"Verbergen","cancel":"Annuleren","ariaShow":"Wachtwoord tonen","ariaHide":"Wachtwoord verbergen"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://bertthe.github.io/CoronaApp/wp-admin/js/user-profile.min.js?ver=5.4.2'></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
